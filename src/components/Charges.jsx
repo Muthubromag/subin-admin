4072c1c15e288c1b7194f0d0d84ab928b0b596d0
BiplDevTeam <development@bromagindia.com>
2024-03-13 07:13:43 +0530

    charges  module build

--- src/components/Charges.jsx
+++ src/components/Charges.jsx
@@ -1,12 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 
-import { Button, Form, Input, Upload, notification } from "antd";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Upload,
+  notification,
+} from "antd";
 import axios from "axios";
 import { get } from "lodash";
 
 function Charges() {
+  const [data, setData] = useState(null);
   const [form] = Form.useForm();
+
+  const fetchCharges = async (load = true) => {
+    try {
+      const result = await axios.get(`${process.env.REACT_APP_URL}/charges`);
+      let charges = get(result, "data.charges", [])?.[0];
+      form.setFieldsValue(charges);
+      setData(charges);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchCharges();
+  }, []);
 
   //   useEffect(() => {
   //     form.setFieldsValue({
@@ -22,90 +46,207 @@ function Charges() {
     try {
       console.log({ value });
 
-      return;
-      //   await axios.post(`${process.env.REACT_APP_URL}/create_footer`, data);
-      //   notification.success({ message: "Footer settings created successfully" });
-      //   fetchData();
+      await axios.post(`${process.env.REACT_APP_URL}/addCharges`, value);
+      setData(value);
+      notification.success({ message: "Charges updated successfully" });
     } catch (err) {}
   };
 
   return (
     <div className="mt-20 pl-3 md:pl-[20vw] text-white">
       <div className="w-[75vw] flex flex-col  lg:flex-row items-center justify-center lg:items-start lg:justify-start gap-5">
-        <div className="flex h-[80vh">
-          <div className="pt-16">
-            <Form
-              form={form}
-              layout="vertical"
-              className="w-[80%] h-[80vh] overflow-y-scroll"
-              onFinish={handleFinish}
-            >
+        <div className="flex w-full">
+          <Form
+            form={form}
+            layout="vertical"
+            className="w-full"
+            onFinish={handleFinish}
+          >
+            <div className="flex flex-row gap-2 items-center mt-2">
               <Form.Item
-                name="gst"
+                name={["gst", "value"]}
                 label={<h1 className="!text-white pl-2">GST %</h1>}
+                rules={[{ type: "number" }]}
+                className="w-[50%]"
+                initialValue={0}
               >
-                <Input
+                <InputNumber
                   type="number"
                   name="gst"
-                  placeholder="gst %"
+                  placeholder="gst"
                   size="large"
+                  min={0}
+                  className="w-full"
+                  defaultValue={0}
                 />
               </Form.Item>
+
               <Form.Item
-                name="delivery"
-                label={<h1 className="!text-white pl-2">Delivery</h1>}
+                label={<h1 className="ps-3 text-white">GST Mode</h1>}
+                name={["gst", "mode"]}
+                className="w-[50%] "
+                initialValue={"percentage"}
               >
-                <Input
+                <Radio.Group name="mode" defaultValue={"percentage"}>
+                  <Radio value="percentage" className="text-white ps-2">
+                    Percentage
+                  </Radio>
+                  <Radio value="fixed" className="text-white ps-1">
+                    Fixed
+                  </Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className="flex flex-row gap-2 items-center mt-2">
+              <Form.Item
+                name={["delivery", "value"]}
+                label={<h1 className="!text-white pl-2">Delivery</h1>}
+                rules={[{ type: "number" }]}
+                className="w-[50%]"
+                initialValue={0}
+              >
+                <InputNumber
                   type="number"
                   name="delivery"
                   placeholder="delivery"
                   size="large"
+                  min={0}
+                  className="w-full"
+                  defaultValue={0}
                 />
               </Form.Item>
+
               <Form.Item
-                name="package"
-                label={<h1 className="!text-white pl-2">Package</h1>}
+                label={<h1 className="ps-3 text-white">Delivery Mode</h1>}
+                name={["delivery", "mode"]}
+                className="w-[50%] "
+                initialValue={"percentage"}
               >
-                <Input
+                <Radio.Group name="mode" defaultValue={"percentage"}>
+                  <Radio value="percentage" className="text-white ps-2">
+                    Percentage
+                  </Radio>
+                  <Radio value="fixed" className="text-white ps-1">
+                    Fixed
+                  </Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className="flex flex-row gap-2 items-center mt-2">
+              <Form.Item
+                name={["packing", "value"]}
+                label={<h1 className="!text-white pl-2">Packing</h1>}
+                rules={[{ type: "number" }]}
+                className="w-[50%]"
+                initialValue={0}
+              >
+                <InputNumber
                   type="number"
-                  name="package"
-                  placeholder="package"
+                  name="packing"
+                  placeholder="packing"
                   size="large"
+                  min={0}
+                  className="w-full"
+                  defaultValue={0}
                 />
               </Form.Item>
+
               <Form.Item
-                name="transaction"
-                label={<h1 className="!text-white pl-2">Transaction</h1>}
+                label={<h1 className="ps-3 text-white">Packing Mode</h1>}
+                name={["packing", "mode"]}
+                className="w-[50%] "
+                initialValue={"percentage"}
               >
-                <Input
+                <Radio.Group name="mode" defaultValue={"percentage"}>
+                  <Radio value="percentage" className="text-white ps-2">
+                    Percentage
+                  </Radio>
+                  <Radio value="fixed" className="text-white ps-1">
+                    Fixed
+                  </Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className="flex flex-row gap-2 items-center mt-2">
+              <Form.Item
+                name={["transaction", "value"]}
+                label={<h1 className="!text-white pl-2">Transaction</h1>}
+                rules={[{ type: "number" }]}
+                className="w-[50%]"
+                initialValue={0}
+              >
+                <InputNumber
                   type="number"
                   name="transaction"
                   placeholder="transaction"
                   size="large"
+                  min={0}
+                  className="w-full"
+                  defaultValue={0}
                 />
               </Form.Item>
+
               <Form.Item
-                name="dining"
-                label={<h1 className="!text-white pl-2">Dining</h1>}
+                label={<h1 className="ps-3 text-white">Transaction Mode</h1>}
+                name={["transaction", "mode"]}
+                className="w-[50%] "
+                initialValue={"percentage"}
               >
-                <Input
+                <Radio.Group name="mode" defaultValue={"percentage"}>
+                  <Radio value="percentage" className="text-white ps-2">
+                    Percentage
+                  </Radio>
+                  <Radio value="fixed" className="text-white ps-1">
+                    Fixed
+                  </Radio>
+                </Radio.Group>
+              </Form.Item>
+            </div>
+            <div className="flex flex-row gap-2 items-center mt-2">
+              <Form.Item
+                name={["dining", "value"]}
+                label={<h1 className="!text-white pl-2">Dining</h1>}
+                rules={[{ type: "number" }]}
+                className="w-[50%]"
+                initialValue={0}
+              >
+                <InputNumber
                   type="number"
                   name="dining"
                   placeholder="dining"
                   size="large"
+                  min={0}
+                  className="w-full"
+                  defaultValue={0}
                 />
               </Form.Item>
 
-              <Form.Item>
-                <Button
-                  htmlType="submit"
-                  className="bg-green-500 text-white w-[150px] float-right"
-                >
-                  Submit
-                </Button>
+              <Form.Item
+                label={<h1 className="ps-3 text-white">Dining Mode</h1>}
+                name={["dining", "mode"]}
+                className="w-[50%] "
+                initialValue={"percentage"}
+              >
+                <Radio.Group name="mode" defaultValue={"percentage"}>
+                  <Radio value="percentage" className="text-white ps-2">
+                    Percentage
+                  </Radio>
+                  <Radio value="fixed" className="text-white ps-1">
+                    Fixed
+                  </Radio>
+                </Radio.Group>
               </Form.Item>
-            </Form>
-          </div>
+            </div>
+
+            <Form.Item>
+              <Button
+                htmlType="submit"
+                className="bg-green-500 text-white w-[150px] float-right"
+              >
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     </div>
