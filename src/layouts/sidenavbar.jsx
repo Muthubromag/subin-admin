e4d3fc70e32a43a541cfcba57de502d986a9d505
Pavithranathbromag <pavithranathbromag@gmail.com>
2024-03-15 14:08:32 +0530

    add side styles

--- src/layouts/sidenavbar.jsx
+++ src/layouts/sidenavbar.jsx
@@ -8,7 +8,6 @@ import { get } from "lodash";
 import { items } from "../helper/menu";
 import { Drawer, Menu } from "antd";
 import { useLocation } from "react-router-dom";
-import MenuIcon from "@mui/icons-material/Menu";
 import LoadingPage from "../components/loadingPage";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
